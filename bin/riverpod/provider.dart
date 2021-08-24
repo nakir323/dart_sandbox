50248908237fa9e59a1273ebce3731c4028a3bed
nakir323 <11908295+nakir323@users.noreply.github.com>
2021-08-24 23:20:51 +0900

    Examle of Provider, ProviderContainer

--- bin/riverpod/provider.dart
+++ bin/riverpod/provider.dart
@@ -3,6 +3,15 @@ import 'package:riverpod/riverpod.dart';
 final helloWorldProvider = Provider((_) => 'Hello world');
 
 void main() {
+  // 最もシンプルなProviderの例
   final value = ProviderContainer().read(helloWorldProvider);
   print(value); // Hello world
+
+  // Providerの値を無理やり変える例
+  // Testのためにモックに切り替えたりするときにProviderScopeを介してこのようなことをするみたい。
+  final container = ProviderContainer(
+      overrides: [helloWorldProvider.overrideWithValue('hello')]);
+  print(container.read(helloWorldProvider)); // hello
+  container.updateOverrides([helloWorldProvider.overrideWithValue('world')]);
+  print(container.read(helloWorldProvider)); // world
 }
